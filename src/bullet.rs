ba44a271084be35b7d61ef3abfee934ccc99a0a7
ittokunvim <73953416+ittokunvim@users.noreply.github.com>
2024-12-10 22:01:22 +0900

    feat: :hammer: Add movement to bullet .rs

--- src/bullet.rs
+++ src/bullet.rs
@@ -1,6 +1,7 @@
 use bevy::prelude::*;
 
 use crate::{
+    WINDOW_SIZE,
     GRID_SIZE,
     PATH_IMAGE_BULLET,
     Player,
@@ -9,6 +10,7 @@ use crate::{
 const IMAGE_SIZE: UVec2 = UVec2::splat(32);
 const COLUMN: u32 = 4;
 const ROW: u32 = 1;
+const SPEED: f32 = 512.0;
 const FPS: f32 = 0.1;
 
 #[derive(Resource, Deref)]
@@ -27,42 +29,9 @@ fn setup(
     mut commands: Commands,
     asset_server: Res<AssetServer>,
 ) {
+    println!("bullet: setup");
     let handle: Handle<Image> = asset_server.load(PATH_IMAGE_BULLET);
     commands.insert_resource(BulletImage(handle));
-}
-
-fn shoot(
-    mut commands: Commands,
-    mut texture_atlas_layouts: ResMut<Assets<TextureAtlasLayout>>,
-    keyboard_input: Res<ButtonInput<KeyCode>>,
-    bullet_image: Res<BulletImage>,
-    player_query: Query<&Transform, With<Player>>,
-) {
-    if !keyboard_input.just_pressed(KeyCode::Space) { return }
-
-    println!("bullet: shoot");
-    let layout = TextureAtlasLayout::from_grid(IMAGE_SIZE, COLUMN, ROW, None, None);
-    let texture_atlas_layout = texture_atlas_layouts.add(layout);
-    let animation_indices = Bullet { first: 0, last: 3, };
-    let player_transform = player_query.single();
-    let translation = Vec3::new(
-        player_transform.translation.x, 
-        player_transform.translation.y + GRID_SIZE * 2.0, 
-        99.0,
-    );
-
-    commands.spawn((
-        Sprite::from_atlas_image(
-            bullet_image.clone(),
-            TextureAtlas {
-                layout: texture_atlas_layout,
-                index: animation_indices.first,
-            },
-        ),
-        Transform::from_translation(translation),
-        animation_indices,
-        AnimationTimer(Timer::from_seconds(FPS, TimerMode::Repeating)),
-    ));
 }
 
 fn animation(
@@ -81,14 +50,73 @@ fn animation(
     }
 }
 
+fn shoot(
+    mut commands: Commands,
+    mut texture_atlas_layouts: ResMut<Assets<TextureAtlasLayout>>,
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    bullet_image: Res<BulletImage>,
+    player_query: Query<&Transform, With<Player>>,
+) {
+    if !keyboard_input.just_pressed(KeyCode::Space) { return }
+
+    let layout = TextureAtlasLayout::from_grid(IMAGE_SIZE, COLUMN, ROW, None, None);
+    let texture_atlas_layout = texture_atlas_layouts.add(layout);
+    let animation_indices = Bullet { first: 0, last: 3, };
+    let player_transform = player_query.single();
+    let translation = Vec3::new(
+        player_transform.translation.x, 
+        player_transform.translation.y + GRID_SIZE * 2.0, 
+        99.0,
+    );
+    // println!("bullet: shoot");
+    commands.spawn((
+        Sprite::from_atlas_image(
+            bullet_image.clone(),
+            TextureAtlas {
+                layout: texture_atlas_layout,
+                index: animation_indices.first,
+            },
+        ),
+        Transform::from_translation(translation),
+        animation_indices,
+        AnimationTimer(Timer::from_seconds(FPS, TimerMode::Repeating)),
+    ));
+}
+
+fn movement(
+    mut query: Query<&mut Transform, With<Bullet>>,
+    time_step: Res<Time<Fixed>>,
+) {
+    for mut transform in &mut query {
+        // move bullet
+        transform.translation.y += SPEED * time_step.delta().as_secs_f32();
+    }
+}
+
+fn despawn(
+    mut commands: Commands,
+    query: Query<(Entity, &Transform), With<Bullet>>,
+) {
+    for (entity, transform) in  &query {
+        if transform.translation.y >= WINDOW_SIZE.y / 2.0 {
+            // println!("bullet: despawn");
+            commands.entity(entity).despawn();
+        }
+    }
+}
+
 pub struct BulletPlugin;
 
 impl Plugin for BulletPlugin {
     fn build(&self, app: &mut App) {
         app
             .add_systems(Startup, setup)
-            .add_systems(Update, shoot)
-            .add_systems(Update, animation)
+            .add_systems(Update, (
+                animation,
+                shoot,
+                movement,
+                despawn,
+            ))
         ;
     }
 }
