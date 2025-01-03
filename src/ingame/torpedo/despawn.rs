34007d02f8bd755d1325eeff2b6efe8a6a7ace88
ittokunvim <73953416+ittokunvim@users.noreply.github.com>
2025-01-03 22:30:22 +0900

    feat: :sparkles: Add ingame/torpedo/despawn.rs
    Issue: #10

---
+++ src/ingame/torpedo/despawn.rs
@@ -0,0 +1,66 @@
+use bevy::prelude::*;
+
+use crate::AppState;
+use crate::ingame::torpedo::TorpedoDespawnEvent;
+use crate::ingame::utils::prelude::*;
+
+const PATH_IMAGE: &str = "bevy-2dshooting-game/torpedo-despawn.png";
+const IMAGE_SIZE: UVec2 = UVec2::splat(64);
+const COLUMN: u32 = 10;
+const ROW: u32 = 1;
+const FPS: f32 = 0.1;
+
+#[derive(Resource, Deref)]
+struct DespawnImage(Handle<Image>);
+
+fn setup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+) {
+    // debug!("setup");
+    let handle: Handle<Image> = asset_server.load(PATH_IMAGE);
+    commands.insert_resource(DespawnImage(handle));
+}
+
+fn spawn(
+    mut commands: Commands,
+    mut texture_atlas_layouts: ResMut<Assets<TextureAtlasLayout>>,
+    mut events: EventReader<TorpedoDespawnEvent>,
+    despawn_image: Res<DespawnImage>,
+) {
+    for event in events.read() {
+        let vec2 = event.0;
+        let layout = TextureAtlasLayout::from_grid(IMAGE_SIZE, COLUMN, ROW, None, None);
+        let texture_atlas_layout = texture_atlas_layouts.add(layout);
+        let translation = Vec3::new(
+            vec2.x, 
+            vec2.y, 
+            99.0,
+        );
+
+        let animation_config = AnimationConfig::new(AnimationName::Despawn, 0, 9, FPS);
+        // debug!("spawn");
+        commands.spawn((
+            Sprite::from_atlas_image(
+                despawn_image.clone(), 
+                TextureAtlas {
+                    layout: texture_atlas_layout,
+                    index: animation_config.first_sprite_index,
+                },
+            ),
+            Transform::from_translation(translation),
+            animation_config,
+        ));
+    }
+}
+
+pub struct DespawnPlugin;
+
+impl Plugin for DespawnPlugin {
+    fn build(&self, app: &mut App) {
+        app
+            .add_systems(OnEnter(AppState::Ingame), setup)
+            .add_systems(Update, spawn.run_if(in_state(AppState::Ingame)))
+        ;
+    }
+}
--- src/ingame/torpedo/mod.rs
+++ src/ingame/torpedo/mod.rs
@@ -2,9 +2,13 @@ use bevy::prelude::*;
 
 pub mod ship;
 pub mod bullet;
+mod despawn;
 
 #[derive(Event)]
 pub struct TorpedoDamageEvent(pub Entity);
+
+#[derive(Event)]
+pub struct TorpedoDespawnEvent(Vec2);
 
 #[derive(Component)]
 pub struct Torpedo {
@@ -19,8 +23,10 @@ impl Plugin for TorpedoPlugin {
     fn build(&self, app: &mut App) {
         app
             .add_event::<TorpedoDamageEvent>()
+            .add_event::<TorpedoDespawnEvent>()
             .add_plugins(bullet::BulletPlugin)
             .add_plugins(ship::ShipPlugin)
+            .add_plugins(despawn::DespawnPlugin)
         ;
     }
 }
