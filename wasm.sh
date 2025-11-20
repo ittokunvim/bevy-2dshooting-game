4d6efe6c0b642f83fbca3228903b130a1649962f
ittokunvim <73953416+ittokunvim@users.noreply.github.com>
2025-11-21 01:21:32 +0900

    fix: :fire: Removed wasm.sh

--- wasm.sh
+++
@@ -1,5 +0,0 @@
-echo "ビルドを開始"
-cargo build --release --target wasm32-unknown-unknown
-
-echo "Wasmに変換"
-wasm-bindgen --target web --out-dir ./examples target/wasm32-unknown-unknown/release/bevy-2dshooting-game.wasm
